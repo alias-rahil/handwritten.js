f5ecc2e4b401f8eff769e91f3d3d7dc9a58cfa65
alias-rahil <rahil.kabani.4@gmail.com>
2020-05-12 18:58:52 +0530

    update

--- src/bin.js
+++ src/bin.js
@@ -10,7 +10,7 @@ async function main(e) {
     const txt = String(fs.readFileSync(e)).trim();
     const output = await handwritten(txt);
     if (output) {
-      output.write('output.jpg');
+      output.resize(output.bitmap.width / 2, output.bitmap.height / 2).quality(50).write('output.jpg');
       console.log("Success! Saved file as 'output.jpg'!");
     } else {
       console.log('ERROR!');
