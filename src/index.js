d79579bdeb49eaceab06057fd0a7d41a35aa3565
alias-rahil <rahil.kabani.4@gmail.com>
2020-08-14 16:12:05 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,58 +4,64 @@ const unidecode = require('unidecode-plus');
 const symbols = '!?"()@&*[]<>{}.,:;-\'';
 const alphanuml = 'qwertyuiopasdfghjklzxcvbnm1234567890';
 const alphanumu = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
 
-async function main(text2) {
-  if (text2.length !== 0) {
-    const text = unidecode(text2);
-    const all = [];
-    let res = [];
-    for (let i = 0; i < text.length; i += 1) {
-      if (alphanuml.includes(text[i])) {
-        res.push(`${__dirname}/dataset/${text[i]}${Math.floor(Math.random() * 6) + 1}.jpg`);
-      } else if (alphanumu.includes(text[i])) {
-        res.push(`${__dirname}/dataset/${Math.floor(Math.random() * 6) + 1}${text[i]}.jpg`);
-      } else if (symbols.includes(text[i])) {
-        res.push(`${__dirname}/dataset/symbol${symbols.indexOf(text[i])}${Math.floor(Math.random() * 6) + 1}.jpg`);
-      } else if (text[i] === ' ') {
-        res.push(`${__dirname}/dataset/space.jpg`);
-      } else if (text[i] === '\n') {
+async function main(raw_text) {
+    if (raw_text.length !== 0) {
+        const text = unidecode(raw_text);
+        const all = [];
+        let res = [];
+        for (let i = 0; i < text.length; i += 1) {
+            if (alphanuml.includes(text[i])) {
+                res.push(`${__dirname}/dataset/${text[i]}${Math.floor(Math.random() * 6) + 1}.jpg`);
+            } else if (alphanumu.includes(text[i])) {
+                res.push(`${__dirname}/dataset/${Math.floor(Math.random() * 6) + 1}${text[i]}.jpg`);
+            } else if (symbols.includes(text[i])) {
+                res.push(`${__dirname}/dataset/symbol${symbols.indexOf(text[i])}${Math.floor(Math.random() * 6) + 1}.jpg`);
+            } else if (text[i] === ' ') {
+                if (countOccurrences(res, `${__dirname}/dataset/space.jpg`) > 5 && [true, false][Math.floor(Math.random() * 2)]) {
+                    all.push(res);
+                    res = [];
+                } else {
+                    res.push(`${__dirname}/dataset/space.jpg`);
+                }
+            } else if (text[i] === '\n') {
+                if (res.length !== 0) {
+                    all.push(res);
+                    res = [];
+                }
+            } else {
+                res.push(`${__dirname}/dataset/unk.jpg`);
+            }
+        }
         if (res.length !== 0) {
-          all.push(res);
-          res = [];
+            all.push(res);
         }
-      } else {
-        res.push(`${__dirname}/dataset/unk.jpg`);
-      }
-    }
-    if (res.length !== 0) {
-      all.push(res);
-    }
-    if (all.length !== 0) {
-      let m = all[0].length;
-      for (let i = 1; i < all.length; i += 1) {
-        if (all[i].length > m) {
-          m = all[i].length;
+        if (all.length !== 0) {
+            let m = all[0].length;
+            for (let i = 1; i < all.length; i += 1) {
+                if (all[i].length > m) {
+                    m = all[i].length;
+                }
+            }
+            for (let i = 0; i < all.length; i += 1) {
+                while (all[i].length !== m) {
+                    all[i].push(`${__dirname}/dataset/space.jpg`);
+                }
+            }
+            const k = [];
+            for (let i = 0; i < all.length; i += 1) {
+                const img = await mergeImg(all[i]);
+                k.push(img);
+            }
+            const img2 = await mergeImg(k, {
+                direction: true,
+            });
+            return img2;
         }
-      }
-      for (let i = 0; i < all.length; i += 1) {
-        while (all[i].length !== m) {
-          all[i].push(`${__dirname}/dataset/space.jpg`);
-        }
-      }
-      const k = [];
-      for (let i = 0; i < all.length; i += 1) {
-        const img = await mergeImg(all[i]);
-        k.push(img);
-      }
-      const img2 = await mergeImg(k, {
-        direction: true,
-      });
-      return img2;
+    } else {
+        return null;
     }
-  } else {
     return null;
-  }
-  return null;
 }
 module.exports = main;
