f4101f7944028441fd16d5bcd5324107dad3a366
alias-rahil <rahil.kabani.4@gmail.com>
2020-08-14 16:22:51 +0530

    update

--- src/bin.js
+++ src/bin.js
@@ -13,21 +13,17 @@ async function main(e) {
       output.write('output.jpg');
       console.log("Success! Saved file as 'output.jpg'!");
     } else {
-      console.log('ERROR!');
-      console.log(`File '${e}' was empty!`);
+      console.log(`Error!\nFile '${e}' was empty!`);
     }
   } catch (err) {
-    console.log('ERROR!');
-    console.log(err);
+    console.log(`Error!\n${err}`);
   }
 }
 
 if (args.length > 1) {
-  console.log('Excessive arguments were provided!');
-  console.log(`Needs 1 arguments but was provided with ${args.length}!`);
+  console.log(`Excess arguments were provided!\nNeeds 1 arguments but was provided with ${args.length}!`);
 } else if (args.length < 1) {
-  console.log('Insufficient arguments were provided!');
-  console.log(`Needs 1 arguments but was provided with ${args.length}!`);
+  console.log(`Insufficient arguments were provided!\nNeeds 1 arguments but was provided with ${args.length}!`);
 } else {
   main(args[0]);
 }
