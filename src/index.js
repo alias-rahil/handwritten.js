2505c27a030c94ba6a8c4efd35cde6706a2ff0e4
alias-rahil <rahil.kabani.4@gmail.com>
2020-08-22 05:19:41 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,92 +1,22 @@
-const unidecode = require('unidecode-plus')
-const mergeImg = require('merge-img')
-const Pdfkit = require('pdfkit')
-const dataset = require('./dataset.json')
-
-function randint() {
+const getbatchsize = function() {
+    let batchsize = 32
+    while ([true, false][Math.floor(Math.random() * 2)]) {
+        batchsize += 1
+    }
+    return batchsize
+}
+const randint = function() {
     return Math.floor(Math.random() * 6)
 }
-async function fillemptyspace(paragraph, width) {
-    for (let i = 0; i < paragraph.length; i += 1) {
-        while (paragraph[i].length !== width) {
-            paragraph[i].push(Buffer.from(dataset['space'][randint()]))
-        }
-    }
-    const k = []
-    for (let i = 0; i < paragraph.length; i += 1) {
-        const img = await mergeImg(paragraph[i])
-        k.push(img)
-    }
-    const blankLine = []
-    while (blankLine.length !== width) {
-        blankLine.push(Buffer.from(dataset['space'][randint()]))
-    }
-    const bl = await mergeImg(blankLine)
-    while (k.length % batchSize !== 0) {
-        k.push(bl)
-    }
-    return k
-}
-
-function getwidth(paragraph) {
-    let width = batchSize
-    for (let i = 0; i < paragraph.length; i += 1) {
-        width = Math.max(paragraph[i].length, width)
-    }
-    return width
-}
-
-function getparagraph(text) {
-    const paragraph = []
-    let line = []
-    for (let i = 0; i < text.length; i += 1) {
-        if (alphanum.includes(text[i])) {
-            line.push(Buffer.from(dataset[text[i]][randint()]))
-        } else if (symbols.includes(text[i])) {
-            line.push(
-                Buffer.from(dataset[`symbol${symbols.indexOf(text[i])}`][randint()])
-            )
-        } else if (text[i] === ' ') {
-            if (line.length > batchSize - 1) {
-                paragraph.push(line)
-                line = []
-            } else {
-                line.push(Buffer.from(dataset['space'][randint()]))
-            }
-        } else if (text[i] === '\n') {
-            paragraph.push(line)
-            line = []
-        }
-    }
-    paragraph.push(line)
-    return paragraph
-}
-
-function getbatchsize() {
-    let batchSize = 32
-    while ([true, false][Math.floor(Math.random() * 2)]) {
-        batchSize += 1
-    }
-    return batchSize
-}
-
-function createbatches(k) {
-    return new Array(Math.ceil(k.length / batchSize)).fill().map((
-            _) => k
-        .splice(0, batchSize))
-}
-
-function cleantext(rawText) {
-    return unidecode(rawText.replace('\t', '    '), {
+const cleantext = function(rawtext) {
+    return unidecode(rawtext.replace('\t', '    '), {
         german: true,
         smartSpacing: true
     }).trim()
 }
-
-function getbufferasync(image, outputType) {
+const getbufferasync = function(image, outputtype) {
     return new Promise((resolve, reject) => {
-        image.getBuffer(`image/${outputType}`, (err,
-            buf) => {
+        image.getBuffer(`image/${outputtype}`, (err, buf) => {
             if (err) {
                 reject(err)
             } else {
@@ -95,11 +25,9 @@ function getbufferasync(image, outputType) {
         })
     })
 }
-
-function getbas64async(image, outputType) {
+const getbas64async = function(image, outputtype) {
     return new Promise((resolve, reject) => {
-        image.getBase64(`image/${outputType}`, (err,
-            buf) => {
+        image.getBase64(`image/${outputtype}`, (err, buf) => {
             if (err) {
                 reject(err)
             } else {
@@ -108,19 +36,17 @@ function getbas64async(image, outputType) {
         })
     })
 }
-async function getimages(result, outputType, cb) {
-    const imgArr = []
-    for (let i = 0; i < result.length; i += 1) {
-        const image = (await mergeImg(result[i], {
-            direction: true
-        })).resize(2380, 3408)
-        imgArr.push(await cb(image, outputType))
+const getwidth = function(paragraph) {
+    let width = batchsize
+    for (let i = 0; i < paragraph.length; i += 1) {
+        if (width < paragraph[i].length) {
+            width = paragraph[i].length
+        }
     }
-    return imgArr
+    return width
 }
-
-function generatepdf(imgArr) {
-    const doc = new Pdfkit({
+const generatepdf = function(imgarr) {
+    const doc = new pdfkit({
         size: [2480, 3508],
         margins: {
             top: 50,
@@ -129,89 +55,142 @@ function generatepdf(imgArr) {
             right: 50
         }
     })
-    for (let i = 0; i < imgArr.length; i += 1) {
-        doc.image(imgArr[i], 50, 50)
-        if (i !== imgArr.length - 1) {
+    for (let i = 0; i < imgarr.length; i += 1) {
+        doc.image(imgarr[i], 50, 50)
+        if (i !== imgarr.length - 1) {
             doc.addPage()
         }
     }
     doc.end()
     return doc
 }
-
-function checkargtype(rawText, outputType) {
-    if (typeof(rawText) !== 'string') {
+const createbatches = function(k) {
+    return new Array(Math.ceil(k.length / batchsize)).fill().map((_) => k
+        .splice(0, batchsize))
+}
+const getimages = async function(result, outputtype, cb) {
+    const imgarr = []
+    for (let i = 0; i < result.length; i += 1) {
+        const image = (await mergeimg(result[i], {
+            direction: true
+        })).resize(2380, 3408)
+        imgarr.push(await cb(image, outputtype))
+    }
+    return imgarr
+}
+const checkargtype = function(rawtext, outputtype) {
+    if (typeof(rawtext) !== 'string') {
         return false
-    } else if (outputType && typeof(outputType) !== 'string') {
+    } else if (outputtype && typeof(outputtype) !== 'string') {
         return false
     } else {
         return true
     }
 }
-async function getRet(text, outputType) {
+const isargvalid = function(outputtype) {
+    return outputtype && !supportedoutputtypes.concat('pdf').includes(
+        outputtype)
+}
+const getparagraph = function(text) {
+    const paragraph = []
+    let line = []
+    for (let i = 0; i < text.length; i += 1) {
+        if (alphanum.includes(text[i])) {
+            line.push(Buffer.from(dataset[text[i]][randint()]))
+        } else if (symbols.includes(text[i])) {
+            line.push(Buffer.from(dataset[
+                `symbol${symbols.indexOf(text[i])}`][randint()]))
+        } else if ((text[i] === ' ' && line.length > batchsize - 2) || text[
+                i] === '\n') {
+            paragraph.push(line)
+            line = []
+        } else {
+            line.push(Buffer.from(dataset['space'][randint()]))
+        }
+    }
+    paragraph.push(line)
+    return paragraph
+}
+const fillemptyspace = async function(paragraph, width) {
+    for (let i = 0; i < paragraph.length; i += 1) {
+        while (paragraph[i].length !== width) {
+            paragraph[i].push(Buffer.from(dataset['space'][randint()]))
+        }
+    }
+    const k = []
+    for (let i = 0; i < paragraph.length; i += 1) {
+        const img = await mergeimg(paragraph[i])
+        k.push(img)
+    }
+    const blankline = []
+    while (blankline.length !== width) {
+        blankline.push(Buffer.from(dataset['space'][randint()]))
+    }
+    const bl = await mergeimg(blankline)
+    while (k.length % batchsize !== 0) {
+        k.push(bl)
+    }
+    return k
+}
+const getret = async function(text, outputtype) {
     const paragraph = getparagraph(text)
     const width = getwidth(paragraph)
     const k = await fillemptyspace(paragraph, width)
     const result = createbatches(k)
-    if (!outputType || outputType === 'pdf') {
-        const type = supportedOutputTypes[Math.floor(Math
-            .random() *
-            supportedOutputTypes.length)]
-        let imgArr
+    if (!outputtype || outputtype === 'pdf') {
+        const type = supportedoutputtypes[Math.floor(Math.random() *
+            supportedoutputtypes.length)]
+        let imgarr
         if (type.slice(-4, 0) === '/b64') {
-            imgArr = await getimages(result, type.slice(0, -4),
+            imgarr = await getimages(result, type.slice(0, -4),
                 getbas64async)
         } else {
-            imgArr = await getimages(result, type.slice(0, -4),
+            imgarr = await getimages(result, type.slice(0, -4),
                 getbufferasync)
         }
-        return generatepdf(imgArr)
+        return generatepdf(imgarr)
     } else {
-        if (outputType.slice(-4, 0) === '/buf') {
-            return await getimages(result, outputType.slice(0, -4),
+        if (outputtype.slice(-4, 0) === '/buf') {
+            return await getimages(result, outputtype.slice(0, -4),
                 getbufferasync)
         } else {
-            return await getimages(result, outputType.slice(0, -4),
+            return await getimages(result, outputtype.slice(0, -4),
                 getbas64async)
         }
     }
 }
-
-function isargvalid(outputType) {
-    return outputType && !supportedOutputTypes.concat('pdf')
-        .includes(
-            outputType)
-}
-const batchSize = getbatchsize()
+const dataset = require('./dataset.json')
+const unidecode = require('unidecode-plus')
+const mergeimg = require('merge-img')
+const pdfkit = require('pdfkit')
 const symbols = '!?"()@&*[]<>{}.,:;-\'~`$#%+\\/|_^='
-const alphanum = 'qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM'
-const supportedOutputTypes = ['jpeg/buf', 'png/buf', 'jpeg/b64',
-    'png/b64'
-]
-async function main(rawText, outputType) {
-    if (!checkargtype(rawText, outputType)) {
+const alphanum =
+    'qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM'
+const supportedoutputtypes = ['jpeg/buf', 'png/buf', 'jpeg/b64', 'png/b64']
+const batchsize = getbatchsize()
+const main = async function(rawtext, outputtype) {
+    if (!checkargtype(rawtext, outputtype)) {
         throw {
             error: 'arguments must be of type string!'
         }
     } else {
-        if (isargvalid(outputType)) {
+        if (isargvalid(outputtype)) {
             throw {
-                error: `Invalid output type "${outputType}"!`,
-                supportedOutputTypes: supportedOutputTypes
-                    .concat(
-                        'pdf'),
+                error: `Invalid output type "${outputtype}"!`,
+                supportedoutputtypes: supportedoutputtypes.concat(
+                    'pdf'),
                 default: 'pdf'
             }
         } else {
-            const text = cleantext(rawText)
+            const text = cleantext(rawtext)
             if (text.length === 0) {
-                if (!outputType || outputType === 'pdf') {
+                if (!outputtype || outputtype === 'pdf') {
                     return generatepdf([])
                 } else {
                     return []
                 }
             } else {
-                return await getRet(text, outputType)
+                return await getret(text, outputtype)
             }
         }
     }
