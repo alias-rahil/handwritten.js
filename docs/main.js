fa90749848184a566d51fcafa84daa04a1bf63f6
alias-rahil <rahil.kabani.4@gmail.com>
2020-10-06 19:28:42 +0530

    fix: clearelemet only once

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,9 @@
+/* eslint no-unused-vars: "off" */
 const text = document.getElementById('text')
 const download = document.getElementById('download')
 const pdf = document.getElementById('pdf')
 const ruled = document.getElementById('ruled')
+let cleared = false
 let w = new Worker('worker.js')
 
 function makepdf () {
@@ -15,8 +17,13 @@ function makepdf () {
   })
 }
 
-function clearContents(element) {
-  element.value = '';
+function clearContents (element) {
+  if (!cleared) {
+    element.value = ''
+    makepdf()
+  } if (!cleared) {
+    cleared = true
+  }
 }
 
 text.addEventListener('input', makepdf)
